2fb8e87f028ba90b79b885b06638c379ad44dd0a
Sampras Lopes <sampraslop995@gmail.com>
2020-05-25 17:54:06 +0530

    Add a small test case

--- TODO/tests.py
+++ TODO/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import TODO
+from .views import update
+
+from django.http import HttpRequest
 
 # Create your tests here.
+
+class TODOModelTests(TestCase):
+
+    def test_todo_was_marked_as_complete(self):
+
+        t = TODO(task="Test",date=timezone.now())
+        t.save()
+        update(HttpRequest(), t.pk)
+        t.refresh_from_db()
+        self.assertIs(t.done, True)
